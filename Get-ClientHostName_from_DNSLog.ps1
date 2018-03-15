ee4f63a0a15f2d97124f86b8ea8bd743bc4fa432
Alexander <32452033+avbor@users.noreply.github.com>
2018-03-15 10:23:39 +0300

    Add Get-ClientHostName_from_DNSLog and Test-Ports_HostsFromFile scripts

---
+++ Get-ClientHostName_from_DNSLog.ps1
@@ -0,0 +1,48 @@
+﻿# Get-ClientHostName_from_DNSLog.ps1
+
+### Settings ###
+$dnsSrv = "10.0.0.1"
+$outFilePath = "dns_clients.txt"
+$logFiles = @(
+    "\\srv-1\c$\DNS_Logs\dns.log",
+    "\\srv-2\c$\DNS_Logs\dns.log",
+    "\\srv-3\c$\DNS_Logs\dns.log"
+)
+################
+
+function GetNames ($logFile, $dnsHost) {
+    Write-Host -NoNewline "Searching for IPs..." -Separator ""
+    $ips = (((Select-String -Path $logFile -Pattern ".* Rcv (\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}) .*").matches | `
+            ForEach-Object {$_.Groups[1].Value}) | Sort-Object | Get-Unique)
+    $names = @()
+    Write-Host -ForegroundColor Green "Done!"
+    Write-Host "Found " $ips.Count " IP addresses" -Separator ""
+    Write-Host -NoNewline "Getting hostnames..."
+    foreach ($ip in $ips) {
+        $names += (Resolve-DnsName -Server $dnsHost -Name $ip -ErrorAction SilentlyContinue).NameHost
+    }
+    Write-Output $names
+    $ips = $null
+    $names = $null
+}
+
+$time = Measure-Command {
+    $Results = @()
+    foreach ($file in $logFiles) {
+        Write-Host "Processing file: " $file " ..." -Separator ""
+        Write-Host "Size:" ((Get-Item -Path $file).Length / 1MB) "MBytes"
+        try {$fileResults = (GetNames -logFile $file -dnsHost $dnsSrv)}
+        catch {Write-Host -ForegroundColor Red $_; Exit}
+        $Results += $fileResults
+        Write-Host -ForegroundColor Green "Done!" -Separator ""
+        Write-Host "Found " $fileResults.Count " hostnames`n" -Separator ""
+        $fileResults = $null
+    }
+}
+
+$Results = $Results | Sort-Object | Get-Unique
+Write-Host "Found total:" $Results.Count
+Write-Host "Time wasted:" $time
+
+$Results | Out-File $outFilePath -Force
+$Results = $null
---
+++ Test-Ports_HostsFromFile.ps1
@@ -0,0 +1,27 @@
+﻿
+Param
+(
+    [Parameter(Position=0,Mandatory = $false)]$Path="hostnames.txt",
+    [Parameter(Position=1,Mandatory = $false)]$Port="22",
+    [Parameter(Position=2,Mandatory = $false)]$Timeout=100
+)
+
+function Test-Port ($addr, $tport, $time) {
+    $tcp = New-Object System.Net.Sockets.TcpClient
+    $conn = $tcp.BeginConnect($addr, $tport, $null, $null)
+    $wait = $conn.AsyncWaitHandle.WaitOne($time, $false) 
+    if (!$wait) {Write-Output $false}
+    else {
+        $error.clear()
+        try {$tcp.EndConnect($conn)}
+        catch {Write-Output $false; return}
+        Write-Output $true
+    }
+}
+
+Write-Output ("Port " + $Port + " is open on the following hosts:`n")
+Get-Content $Path | ForEach-Object {
+    if((Test-Port -addr $_ -tport $Port -time $Timeout)) {
+        $_
+    }
+}
